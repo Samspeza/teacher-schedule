50b30d97569d350d178085329c9a0ee9ba32aa4e
Samspeza <sanvic77@gmail.com>
2025-02-19 14:39:05 -0300

    Inserção de Crud

--- style.py
+++ style.py
@@ -1,94 +1,19 @@
-# style.py
-import tkinter as tk
-# Cores
-BACKGROUND_COLOR = "#F4F6F9"
-BUTTON_COLOR = "#00796B"
-BUTTON_TEXT_COLOR = "white"
-LABEL_COLOR = "#B2DFDB"
-TEXT_COLOR = "black"
-WHITE_COLOR = "#FFFFFF"
-
-# Fontes
+# Definindo cores
+BACKGROUND_COLOR = "#ffffff"  
+BUTTON_COLOR = ""  
+BUTTON_TEXT_COLOR = "#333333" 
+TEXT_COLOR = "#333333"  
+LABEL_COLOR = "#f9f9f9"  
+WHITE_COLOR = "#ffffff"  
+HEADER_COLOR = "#eeeeee" 
+TEACHER_FONT = ("Arial", 9) 
+LABEL_FONT = ("Arial", 10) 
+HEADER_FONT = ("Arial", 14, "bold")  
+TIME_SLOT_FONT = ("Arial", 9, "italic")  
 HEADER_FONT = ("Arial", 18, "bold")
-LABEL_FONT = ("Arial", 12, "bold")
-TIME_SLOT_FONT = ("Arial", 12)
-TEACHER_FONT = ("Arial", 10)
 
-def style_tkinter_widgets(root):
-    root.config(bg="#f5f5f5")
-    root.geometry("1200x600")
-    
-    def style_frame(frame):
-        frame.config(bg="#ffffff", relief="solid", borderwidth=1)
-        
-    def style_label(label, font_size=12, font_weight="normal", bg_color="#f5f5f5", fg_color="black"):
-        label.config(font=("Arial", font_size, font_weight), bg=bg_color, fg=fg_color)
-    
-    return style_frame, style_label
 
-def style_tkinter_widgets(root):
-    root.config(bg="#f5f5f5")
-    root.geometry("1200x600")
-    
-    def style_frame(frame):
-        frame.config(bg="#ffffff", relief="solid", borderwidth=1)
-        
-    def style_label(label, font_size=12, font_weight="normal", bg_color="#f5f5f5", fg_color="black"):
-        label.config(font=("Arial", font_size, font_weight), bg=bg_color, fg=fg_color)
-    
-    return style_frame, style_label
 
-def display_timetable_gui(timetables, days_of_week, time_slots, teachers):
-    root = tk.Tk()
-    root.title("Grade de Aulas")
-    
-    style_frame, style_label = style_tkinter_widgets(root)
 
-    canvas = tk.Canvas(root, bg="#F4F6F9")
-    canvas.pack(side="left", fill="both", expand=True)
 
-    scrollbar = tk.Scrollbar(root, orient="vertical", command=canvas.yview)
-    scrollbar.pack(side="right", fill="y")
 
-    canvas.config(yscrollcommand=scrollbar.set)
-
-    timetable_frame = tk.Frame(canvas, bg="#F4F6F9")
-    canvas.create_window((0, 0), window=timetable_frame, anchor="nw")
-
-    def create_class_table(name, timetable_class):
-        frame = tk.Frame(timetable_frame, bg="#ffffff", relief="flat", borderwidth=0)
-        frame.pack(padx=20, pady=20, fill="x", expand=True)
-        style_frame(frame)
-
-        label = tk.Label(frame, text=f"Grade para {name}", font=("Arial", 18, "bold"), bg="#00796B", fg="white")
-        style_label(label, font_size=18, font_weight="bold", bg_color="#00796B", fg_color="white")
-        label.grid(row=0, column=0, columnspan=len(days_of_week) + 1, pady=10)
-
-        for i, day in enumerate(days_of_week):
-            label = tk.Label(frame, text=day, font=("Arial", 12, "bold"), bg="#00796B", fg="white")
-            style_label(label, font_size=12, font_weight="bold", bg_color="#00796B", fg_color="white")
-            label.grid(row=1, column=i+1, padx=10, pady=10)
-
-        for row, time_slot in enumerate(time_slots, start=2):
-            label = tk.Label(frame, text=time_slot, font=("Arial", 12), bg="#B2DFDB", fg="black")
-            style_label(label, font_size=12, font_weight="normal", bg_color="#B2DFDB", fg_color="black")
-            label.grid(row=row, column=0, padx=15, pady=5)
-
-            for col, day in enumerate(days_of_week, start=1):
-                teacher = timetable_class[day].get(time_slot, None)
-                teacher_label = tk.Label(frame, text=teacher, font=("Arial", 10), bg="#FFFFFF", fg="black")
-                style_label(teacher_label, font_size=10, font_weight="normal", bg_color="#FFFFFF", fg_color="black")
-                teacher_label.grid(row=row, column=col, padx=10, pady=5)
-
-    def update_timetable(timetable):
-        for widget in timetable_frame.winfo_children():
-            widget.destroy()
-
-        for name, timetable_class in timetable.items():
-            create_class_table(name, timetable_class)
-
-        timetable_frame.update_idletasks()
-        canvas.config(scrollregion=canvas.bbox("all"))
-
-    update_timetable(timetables)
-    root.mainloop()
--- teacher-schedule.py
+++ teacher-schedule.py
@@ -1,146 +1,199 @@
 import tkinter as tk
 from tkinter import ttk
 import random
-import sqlite3
+from tkinter import PhotoImage
+from tkinter.messagebox import askyesno
 from style import *
-from DbContext.models import get_teachers, update_teacher_availability, get_teacher_availability
 from config import teachers, teacher_limits, classes, days_of_week, time_slots
-from DbContext.database import DB_NAME
 
-def get_teachers_from_db():
-    return list(teachers.keys())
-
-teachers = get_teachers_from_db()
-
-teacher_allocations = {teacher: set() for teacher in teachers}
-
-def generate_timetable():
-    timetable = {cls: {day: {time_slot: None for time_slot in time_slots} for day in days_of_week} for cls in classes}
-
-    for cls in classes:
-        for day in days_of_week:
-            previous_teacher = None
-            for i, time_slot in enumerate(time_slots):
-                available_teachers = [teacher for teacher in teachers
-                                      if teacher not in teacher_limits or len(teacher_allocations[teacher]) < teacher_limits.get(teacher, float('inf'))]
-
-                if time_slot == "20:25 - 20:45": 
-                    teacher = "INTERVALO"
-                elif not available_teachers:
-                    teacher = "[SEM PROFESSOR]"
-                else:
-                    if previous_teacher and previous_teacher in available_teachers and random.random() < 0.2:
-                        teacher = previous_teacher
-                    else:
-                        teacher = random.choice([t for t in available_teachers if t != previous_teacher] or available_teachers)
-                        teacher_allocations[teacher].add(day)
-
-                timetable[cls][day][time_slot] = teacher
-                previous_teacher = teacher
-    return timetable
-
-def update_teacher_in_db(name, day, time_slot, new_teacher):
-    conn = sqlite3.connect("schedule.db")
-    teacher_id = None
-    for teacher in get_teachers():  
-        if teacher[1] == new_teacher:  
-            teacher_id = teacher[0]
-            break
-
-    if teacher_id:
-        update_teacher_availability(teacher_id, day, new_teacher)  
-
-    conn.close()
-
-def display_timetable_gui(timetables):
-    root = tk.Tk()
-    root.title("Grade de Aulas")
-    root.config(bg=BACKGROUND_COLOR)
+class TimetableApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Gerenciamento de Grade de Aulas")
+        self.root.geometry("800x600")
+        self.root.config(bg=BACKGROUND_COLOR)
+        self.selected_cell = None
+        self.teacher_allocations = {teacher: set() for teacher in teachers}
+        
+        self.setup_ui()
     
-    canvas = tk.Canvas(root, bg=BACKGROUND_COLOR)
-    canvas.pack(side="left", fill="both", expand=True)
+    def setup_ui(self):
+        title_label = tk.Label(self.root, text="Gerenciamento de Grade de Aulas", font=HEADER_FONT, bg=HEADER_COLOR, fg=TEXT_COLOR)
+        title_label.pack(pady=10)
+        
+        self.button_frame = tk.Frame(self.root, bg=BACKGROUND_COLOR)
+        self.button_frame.pack(pady=10)
+        
+        self.save_icon = PhotoImage(file=r"icons\salvar.png").subsample(20, 20)  
+        self.cancel_icon = PhotoImage(file=r"icons\cancel.png").subsample(20, 20)  
+        
+        self.list_icon = PhotoImage(file="icons/list.png").subsample(20, 20)  
+        self.edit_icon = PhotoImage(file="icons/edit.png").subsample(20, 20)  
+        self.delete_icon = PhotoImage(file="icons/delete.png").subsample(20, 20)  
+        
+        self.list_button = tk.Button(self.button_frame, image=self.list_icon, command=self.show_timetable, padx=8, pady=4)
+        self.list_button.pack(side="left", padx=8)
+        
+        self.edit_button = tk.Button(self.button_frame, image=self.edit_icon, command=self.edit_teacher, state="disabled", padx=8, pady=4)
+        self.edit_button.pack(side="left", padx=8)
+        
+        self.save_button = tk.Button(self.button_frame, image=self.save_icon, command=self.save_changes, state="disabled", padx=8, pady=4)
+        self.save_button.pack(side="left", padx=8)
 
-    scrollbar = tk.Scrollbar(root, orient="vertical", command=canvas.yview)
-    scrollbar.pack(side="right", fill="y")
-
-    canvas.config(yscrollcommand=scrollbar.set)
-
-    timetable_frame = tk.Frame(canvas, bg=BACKGROUND_COLOR)
-    canvas.create_window((0, 0), window=timetable_frame, anchor="nw")
-
-    def create_class_table(name, timetable_class):
-        frame = tk.Frame(timetable_frame, bg=WHITE_COLOR, relief="flat", borderwidth=0)
-        frame.pack(padx=20, pady=20, fill="x", expand=True)
-
-        label = tk.Label(frame, text=f"Grade para {name}", font=HEADER_FONT, bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR)
-        label.grid(row=0, column=0, columnspan=len(days_of_week) + 1, pady=10)
-
+        self.cancel_button = tk.Button(self.button_frame, image=self.cancel_icon, command=self.cancel_edit, state="disabled", padx=8, pady=4)
+        self.cancel_button.pack(side="left", padx=8)
+        
+        self.delete_button = tk.Button(self.button_frame, image=self.delete_icon, command=self.confirm_delete_schedule, state="disabled", padx=8, pady=4)
+        self.delete_button.pack(side="left", padx=8)
+        
+        self.timetable_frame = tk.Frame(self.root, bg=BACKGROUND_COLOR)
+        self.timetable_frame.pack(fill="both", expand=True, padx=10, pady=10)
+        
+        self.scroll_canvas = tk.Canvas(self.timetable_frame)
+        self.scroll_frame = tk.Frame(self.scroll_canvas, bg=BACKGROUND_COLOR)
+        self.scroll_bar = tk.Scrollbar(self.timetable_frame, orient="vertical", command=self.scroll_canvas.yview)
+        self.scroll_canvas.configure(yscrollcommand=self.scroll_bar.set)
+        
+        self.scroll_bar.pack(side="right", fill="y")
+        self.scroll_canvas.pack(side="left", fill="both", expand=True)
+        self.scroll_canvas.create_window((0, 0), window=self.scroll_frame, anchor="nw")
+        self.scroll_frame.bind(
+            "<Configure>",
+            lambda e: self.scroll_canvas.configure(scrollregion=self.scroll_canvas.bbox("all"))
+        )
+    
+    def generate_timetable(self):
+        timetable = {cls: {day: {time_slot: None for time_slot in time_slots} for day in days_of_week} for cls in classes}
+    
+        for cls in classes:
+            for day in days_of_week:
+                previous_teacher = None
+                for i, time_slot in enumerate(time_slots):
+                    available_teachers = [
+                        teacher for teacher in teachers
+                        if teacher not in teacher_limits or len(self.teacher_allocations[teacher]) < teacher_limits.get(teacher, float('inf'))
+                    ]
+    
+                    if time_slot == "20:25 - 20:45": 
+                        teacher = "INTERVALO"
+                    elif not available_teachers:
+                        teacher = "[SEM PROFESSOR]"
+                    else:
+                        if previous_teacher and previous_teacher in available_teachers and random.random() < 0.2:
+                            teacher = previous_teacher
+                        else:
+                            teacher = random.choice([t for t in available_teachers if t != previous_teacher] or available_teachers)
+                            self.teacher_allocations[teacher].add(day)
+                    
+                    timetable[cls][day][time_slot] = teacher
+                    previous_teacher = teacher
+        return timetable
+    
+    def show_timetable(self):
+        for widget in self.scroll_frame.winfo_children():
+            widget.destroy()
+        
+        self.timetable = self.generate_timetable()
+        
+        for name, timetable_class in self.timetable.items():
+            self.create_class_table(self.scroll_frame, name, timetable_class)
+    
+    def create_class_table(self, parent, name, timetable_class):
+        frame = tk.Frame(parent, bg=WHITE_COLOR, relief="solid", borderwidth=1)
+        frame.pack(padx=15, pady=10, fill="x", expand=True)
+        
+        label = tk.Label(frame, text=f"Grade para {name}", font=HEADER_FONT, bg=HEADER_COLOR, fg=TEXT_COLOR)
+        label.grid(row=0, column=0, columnspan=len(days_of_week) + 1, pady=5)
+        
         for i, day in enumerate(days_of_week):
-            label = tk.Label(frame, text=day, font=LABEL_FONT, bg=BUTTON_COLOR, fg=BUTTON_TEXT_COLOR)
-            label.grid(row=1, column=i+1, padx=10, pady=10)
-
+            tk.Label(frame, text=day, font=LABEL_FONT, bg=HEADER_COLOR, fg=TEXT_COLOR).grid(row=1, column=i+1, padx=6, pady=6)
+        
         for row, time_slot in enumerate(time_slots, start=2):
-            label = tk.Label(frame, text=time_slot, font=TIME_SLOT_FONT, bg=LABEL_COLOR, fg=TEXT_COLOR)
-            label.grid(row=row, column=0, padx=15, pady=5)
-
+            tk.Label(frame, text=time_slot, font=TIME_SLOT_FONT, bg=LABEL_COLOR, fg=TEXT_COLOR).grid(row=row, column=0, padx=10, pady=3)
             for col, day in enumerate(days_of_week, start=1):
                 teacher = timetable_class[day].get(time_slot, None)
-                teacher_label = tk.Label(frame, text=teacher, font=TEACHER_FONT, bg=WHITE_COLOR, fg=TEXT_COLOR)
-                teacher_label.grid(row=row, column=col, padx=10, pady=5)
+                cell_label = tk.Label(frame, text=teacher, font=TEACHER_FONT, bg=WHITE_COLOR, fg=TEXT_COLOR, padx=8, pady=4, relief="groove", borderwidth=1)
+                cell_label.grid(row=row, column=col, padx=6, pady=3)
+                cell_label.bind("<Button-1>", lambda e, d=day, t=time_slot, l=cell_label: self.select_cell(d, t, l))
+    
+    def select_cell(self, day, time_slot, label):
+        if self.selected_cell:
+            self.selected_cell.config(bg=WHITE_COLOR)
+        
+        self.selected_cell = label
+        self.selected_cell.config(bg=LABEL_COLOR)
+        
+        self.original_teacher = self.selected_cell.cget("text")  
+        self.edit_button.config(state="normal")
+        self.save_button.config(state="normal")
+        self.cancel_button.config(state="normal")
+        self.delete_button.config(state="normal")
+    
+    def edit_teacher(self):
+        if not self.selected_cell:
+            return
+        
+        self.original_teacher = self.selected_cell.cget("text") 
+        self.open_teacher_modal()
+    
+    def open_teacher_modal(self):
+        modal_window = tk.Toplevel(self.root)
+        modal_window.title("Escolher Novo Professor")
+        
+        label = tk.Label(modal_window, text="Selecione um novo professor:")
+        label.pack(pady=8)
+        
+        teacher_select = ttk.Combobox(modal_window, values=list(teachers.keys()))  
+        teacher_select.set(self.selected_cell.cget("text"))  
 
-                edit_button = tk.Button(frame, text="Editar", command=lambda cls=name, d=day, ts=time_slot, teacher=teacher, label=teacher_label: edit_teacher(cls, d, ts, teacher, label))
-                edit_button.grid(row=row, column=col+1, padx=5)
-
-    def edit_teacher(name, day, time_slot, teacher, teacher_label):
-        edit_window = tk.Toplevel(root)
-        edit_window.title(f"Editar Professor - {name} - {day} - {time_slot}")
-
-        label = tk.Label(edit_window, text="Selecione um novo professor:")
-        label.pack(pady=10)
-
-        teacher_select = ttk.Combobox(edit_window, values=teachers)
-        teacher_select.set(teacher)  
-        teacher_select.pack(pady=10)
-
-        def save_changes():
+        teacher_select.pack(pady=8)
+        
+        def save_teacher():
             new_teacher = teacher_select.get()
-            if new_teacher and new_teacher != teacher:
+            self.selected_cell.config(text=new_teacher)  
+            modal_window.destroy()
+            self.save_button.config(state="disabled")
+            self.cancel_button.config(state="disabled")
+        
+        def cancel():
+            modal_window.destroy()
+            self.selected_cell.config(text=self.original_teacher)  
+        
+        self.save_button.config(command=save_teacher, state="normal")
+        self.cancel_button.config(command=cancel, state="normal")
+    
+    def save_changes(self):
+        if self.selected_cell:
+            new_teacher = self.selected_cell.cget("text")  
+            self.selected_cell.config(text=new_teacher) 
+        
+            self.save_button.config(state="disabled")
+            self.cancel_button.config(state="disabled")
+    
+    def cancel_edit(self):
+        if self.selected_cell:
+            self.selected_cell.config(text=self.original_teacher)  
+        
+        self.save_button.config(state="disabled")
+        self.cancel_button.config(state="disabled")
+    
+    def confirm_delete_schedule(self):
+        if self.selected_cell:
+            confirmation = askyesno("Confirmar Exclusão", "Você tem certeza que deseja excluir este registro?")
+            if confirmation:
+                self.delete_schedule()
+    
+    def delete_schedule(self):
+        if self.selected_cell:
+            self.selected_cell.config(text="[REMOVIDO]")
+            self.selected_cell.config(bg=WHITE_COLOR)
+            self.selected_cell = None
+            self.edit_button.config(state="disabled")
+            self.delete_button.config(state="disabled")
+            self.save_button.config(state="disabled")
+            self.cancel_button.config(state="disabled")
 
-                update_teacher_in_db(name, day, time_slot, new_teacher)
-                edit_window.destroy()
-
-                teacher_label.config(text=new_teacher)
-
-        save_button = tk.Button(edit_window, text="Salvar", command=save_changes)
-        save_button.pack(pady=20)
-
-    def generate_and_update_timetable():
-        timetables = generate_timetable()
-        update_timetable(timetables)  
-
-    def update_timetable(timetable):
-        for widget in timetable_frame.winfo_children():
-            widget.destroy()
-
-        for name, timetable_class in timetable.items():
-            create_class_table(name, timetable_class)
-
-        timetable_frame.update_idletasks()
-        canvas.config(scrollregion=canvas.bbox("all"))
-
-    timetables = [generate_timetable() for _ in range(5)]
-    solution_number = 0
-
-    def show_next_solution():
-        nonlocal solution_number
-        solution_number = (solution_number + 1) % 5
-        update_timetable(timetables[solution_number])
-
-    next_button = tk.Button(root, text="Próxima Solução", command=show_next_solution)
-    next_button.pack(pady=20)
-
-    update_timetable(timetables[0])
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TimetableApp(root)
     root.mainloop()
-
-display_timetable_gui(generate_timetable())
-
