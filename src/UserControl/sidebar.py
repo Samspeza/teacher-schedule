a8a0f4f691fa73c7a2879a6f01029f5f9be5d223
Samspeza <sanvic77@gmail.com>
2025-02-24 09:31:17 -0300

    #

--- src/UserControl/sidebar.py
+++ src/UserControl/sidebar.py
@@ -5,8 +5,7 @@ def create_sidebar(root, show_home_screen, show_modules_screen, save_changes):
     sidebar_frame = tk.Frame(root, bg="#2A72C3", width=200)
     sidebar_frame.pack(side="left", fill="y")
     sidebar_frame.pack_propagate(False)
-
-    # Botão de graduação 🎓
+    
     graduation_button = tk.Button(
         sidebar_frame,
         text="🎓",
@@ -20,10 +19,8 @@ def create_sidebar(root, show_home_screen, show_modules_screen, save_changes):
     )
     graduation_button.pack(pady=10)
 
-    # Ícone do botão "Módulos"
     module_icon = PhotoImage(file="icons/list.png").subsample(15, 15)
 
-    # Botão de Módulos
     module_button = tk.Button(
         sidebar_frame,
         image=module_icon,
@@ -40,11 +37,9 @@ def create_sidebar(root, show_home_screen, show_modules_screen, save_changes):
     )
     module_button.pack(pady=10, padx=10, fill="x")
 
-    # Frame onde ficam as opções de módulos
     modules_frame = tk.Frame(sidebar_frame, bg="#1F5AA9")
-    modules_frame.pack_forget()  # Esconde inicialmente
+    modules_frame.pack_forget() 
 
-    # Botão "Salvar Grades"
     save_grades_button = tk.Button(
         modules_frame,
         text="Salvar Grades",
@@ -60,7 +55,7 @@ def create_sidebar(root, show_home_screen, show_modules_screen, save_changes):
     return sidebar_frame
 
 def toggle_modules(modules_frame):
-    if modules_frame.winfo_ismapped():  # Se já está visível, esconder
+    if modules_frame.winfo_ismapped():  
         modules_frame.pack_forget()
-    else:  # Se não está visível, mostrar
+    else: 
         modules_frame.pack(fill="x", padx=10)
