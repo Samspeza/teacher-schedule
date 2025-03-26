5fe4c6f0c378f1ba524caa34905ab5e3a150560b
Samspeza <sanvic77@gmail.com>
2025-03-26 13:59:48 -0300

    edição de usuario

--- src/ScreenManager.py
+++ src/ScreenManager.py
@@ -1,7 +1,9 @@
 import tkinter as tk
+from tkinter import PhotoImage
 from Pages.manageDisciplines import ManageSubjectsApp
 from Pages.saved_grades import SavedGradesApp
 from Pages.manageTeachers import ManageTeachersApp
+from Pages.userProfileApp import UserProfileApp  
 import sys
 import os
 from DbContext.database import DB_NAME
@@ -15,6 +17,18 @@ class ScreenManager:
         self.root.title("Módulo Menu")
         self.root.geometry("900x800")
         self.root.configure(bg="#F8F8F8")
+        
+        # Frame do cabeçalho
+        header_frame = tk.Frame(self.root, bg="#F8F8F8")
+        header_frame.pack(fill=tk.X, padx=20, pady=10)
+        
+        self.user_icon = tk.PhotoImage(file="icons/list.png").subsample(10, 10)  # Reduzindo pela metade
+        self.user_button = tk.Button(
+            header_frame, image=self.user_icon, command=self.open_user_profile, 
+            bg="#F8F8F8", bd=0, cursor="hand2"
+        )
+        self.user_button.pack(side=tk.RIGHT)
+
         
         # Frame central
         central_frame = tk.Frame(self.root, bg="#F8F8F8")
@@ -38,15 +52,16 @@ class ScreenManager:
 
         self.subjects_button = tk.Button(central_frame, text="CADASTRAR DISCIPLINA", font=("Arial", 16), command=self.open_subjects, bg="#2A72C3", fg="white", width=20, height=2)
         self.subjects_button.pack(pady=20)
-
-    def show_timetable_screen(self):
-        """Exibe a tela do horário do coordenador"""
-        from Pages.teacherschedule import TimetableApp
-        self.clear_screen()
-        self.timetable_app = TimetableApp(self.root, self.coordinator_id)  
+    
+    def open_user_profile(self):
+        """Abre a tela de edição de perfil do usuário."""
+        self.root.destroy()
+        user_root = tk.Tk()
+        user_app = UserProfileApp(user_root, self.coordinator_id)
+        user_root.mainloop()
 
     def open_timetable(self):
-        """Abre a tela de criação de grade"""
+        """Abre a tela de criação de grade."""
         self.root.destroy()
         from Pages.teacherschedule import TimetableApp
 
@@ -55,21 +70,21 @@ class ScreenManager:
         timetable_root.mainloop()
 
     def open_saved(self):
-        """Abre a tela de grades salvas"""
+        """Abre a tela de grades salvas."""
         self.root.destroy()
         saved_root = tk.Tk()
         saved_app = SavedGradesApp(saved_root, self.coordinator_id)
         saved_root.mainloop()
 
     def open_teachers(self):
-        """Abre a tela de gerenciamento de professores"""
+        """Abre a tela de gerenciamento de professores."""
         self.root.destroy()
         teachers_root = tk.Tk()
         teachers_app = ManageTeachersApp(teachers_root, self.coordinator_id)
         teachers_root.mainloop()
 
     def open_subjects(self):
-        """Abre a tela de cadastro de disciplinas"""
+        """Abre a tela de cadastro de disciplinas."""
         self.root.destroy()
         subjects_root = tk.Tk()
         subjects_app = ManageSubjectsApp(subjects_root, self.coordinator_id) 
@@ -77,5 +92,5 @@ class ScreenManager:
 
 if __name__ == "__main__": 
     root = tk.Tk()
-    app = ScreenManager(root)
+    app = ScreenManager(root)  
     root.mainloop()
